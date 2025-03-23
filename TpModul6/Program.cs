8253a62d6707d2a90f7c9e1a06f6d75219217545
maxswin99 <andreaschristianfirga06@gmail.com>
2025-03-23 22:12:21 +0700

    Update Program.cs

--- TpModul6/Program.cs
+++ TpModul6/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    public int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Review Film - Andraes Christian Firga");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
