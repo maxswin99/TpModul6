0a8065b7966fab40b3f0ac553337ec02f84cbed2
maxswin99 <andreaschristianfirga06@gmail.com>
2025-03-23 22:23:13 +0700

    Update Program.cs

--- TpModul6/Program.cs
+++ TpModul6/Program.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Diagnostics;
 
 public class SayaTubeVideo
 {
     private int id;
     private string title;
-    public int playCount;
+    private int playCount;
 
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(title != null && title.Length <= 100, "Judul tidak boleh null dan maksimal 100 karakter!");
+
         Random rand = new Random();
         this.id = rand.Next(10000, 99999);
         this.title = title;
@@ -16,7 +19,11 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
-        playCount += count;
+        Debug.Assert(count > 0 && count <= 10000000, "Input play count maksimal 10 juta per panggilan!");
+        checked
+        {
+            playCount += count;
+        }
     }
 
     public void PrintVideoDetails()
@@ -31,8 +38,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Review Film - Andraes Christian Firga");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Andreas Christian Firga");
         video.IncreasePlayCount(100);
         video.PrintVideoDetails();
+
+        Console.ReadKey(); // Agar output tidak langsung tertutup
     }
 }
